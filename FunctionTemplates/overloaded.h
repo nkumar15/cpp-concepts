2415f17b552b2d40b928cb09fd26e91bc52a3413
nkumar15 <nir.neeraj@gmail.com>
2016-05-14 23:29:11 +0800

    code

--- FunctionTemplates/overloaded.h
+++ FunctionTemplates/overloaded.h
@@ -5,11 +5,11 @@
 /* Function 1 */
 template<typename T>
 const& T max(const T& a, const T& b){
-    return a > b? a : b;
+    return a > b ? a : b;
 }
 
 int max (int a, int b){
-    return a > b : a ? b;
+    return a > b ?a : b;
 }
 
 #endif // OVERLOADED_H_INCLUDED
