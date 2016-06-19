8e9f0b5b4be44609c0d2c49805c93d32ba5a869b
nkumar15 <nir.neeraj@gmail.com>
2016-06-19 17:50:31 +0800

    variadic templates

--- VariadicTemplates/main.cpp
+++ VariadicTemplates/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <tuple>
+
 using namespace std;
 
 template<typename T>
@@ -8,11 +8,9 @@ void show_args(T &val)
     cout<<val<<" ";
 }
 
-
 template<typename U, typename... T>
 void show_args(U val, T... args)
 {
-    //int num_args = sizeof...(args);
     cout<<val <<" ";
     show_args(args...);
 }
@@ -29,5 +27,6 @@ int main()
 Value 1 is being passed to val (U val) in function
 2.3, 3.4 and hello are passed as variadic arguments
 so again variadic show_args gets called in which val becomes 2.3 and 3.4 and hello becomes args
-This cycle continues until it reaches last case when it is left with 1 argument hello */
+This cycle continues until it reaches last case when it is left with 1 argument hello
+*/
 
